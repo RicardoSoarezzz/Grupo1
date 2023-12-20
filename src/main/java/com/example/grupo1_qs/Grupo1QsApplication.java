8a1e677708e4150c4f78fce3d7be58fb99cbf0b0
Ricardo Soares <105425605+RicardoSoarezzz@users.noreply.github.com>
2023-12-20 09:18:54 +0000

    Base

---
+++ src/main/java/com/example/grupo1_qs/Grupo1QsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo1_qs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1QsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1QsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupo1_qs/Grupo1QsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo1_qs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo1QsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
